f8d93ca996e4a7926ca9f1d923a7e69b4c2d1ed0
freezedcheese <58059756+freezedcheese@users.noreply.github.com>
2023-01-14 18:49:50 -0500

    m

--- ad_to_product.py
+++ ad_to_product.py
@@ -6,32 +6,42 @@ from pytesseract import Output
 print("Hello")
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\nikhi\AppData\Local\Programs\Tesseract-OCR\tesseract'
 img = cv2.imread('ad1.png')
+img = cv2.resize(img, None, fx=2, fy=2)
 
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
+# def get_grayscale(image):
+#     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# # noise removal
+# def remove_noise(image):
+#     return cv2.medianBlur(image,5)
  
-#thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+# #thresholding
+# def thresholding(image):
+#     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-gray = get_grayscale(img)
-thresh = thresholding(gray)
-text = pytesseract.image_to_string(img)
+# gray = get_grayscale(img)
+# thresh = thresholding(gray)
+# text = pytesseract.image_to_string(img)
 
-ocr_output_details = pytesseract.image_to_data(img, output_type = Output.DICT, lang='eng')
-# Total bounding boxes
-n_boxes = len(ocr_output_details['level'])
-print(n_boxes)
-print(ocr_output_details)
-box_size= []
-for i in range(n_boxes):
-    (x, y, w, h) = (ocr_output_details['left'][i], ocr_output_details['top'][i], ocr_output_details['width'][i], ocr_output_details['height'][i])
-    box_size.append((abs(x-w)*abs(y-h)))
+# ocr_output_details = pytesseract.image_to_data(img, output_type = Output.DICT, lang='eng')
+# # Total bounding boxes
+# n_boxes = len(ocr_output_details['level'])
+# print(n_boxes)
+# print(ocr_output_details)
+# box_size= []
+# for i in range(n_boxes):
+#     (x, y, w, h) = (ocr_output_details['left'][i], ocr_output_details['top'][i], ocr_output_details['width'][i], ocr_output_details['height'][i])
+#     box_size.append((abs(x-w)*abs(y-h)))
 
 
-print(box_size.index(max(box_size)))
-print(text)
+# print(box_size.index(max(box_size)))
+# print(text)
+
+
+data = pytesseract.image_to_string(img)
+print(data)
+
+
+
+
